f6bce78775a6a48ac841980d89581753249d927d
Brocode154 <166228507+RonakF13@users.noreply.github.com>
2026-06-23 14:50:31 +0530

    Fix formatting issue in copyProps function

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
+  if (from && typeof from === "object" || typeof from === "function") {  
     for (let key of __getOwnPropNames(from))
       if (!__hasOwnProp.call(to, key) && key !== except)
         __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
